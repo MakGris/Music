9ac521e3b633f46769b694fedcb819e26f57c405
MakGris <maxgrishenko@gmail.com>
2022-10-07 19:40:57 +0700

    Add ViewControllers

---
+++ MusicApp/MainTabBarController.swift
@@ -0,0 +1,46 @@
+//
+//  MainTabBarController.swift
+//  MusicApp
+//
+//  Created by Maksim Grischenko on 07.10.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.backgroundColor = .white
+        
+        tabBar.tintColor = UIColor(
+            displayP3Red: 253/255,
+            green: 45/255,
+            blue: 85/255,
+            alpha: 1
+        )
+        viewControllers = [generateViewController(
+            rootViewController: SearchViewController(),
+            image: UIImage(named: "Search") ?? UIImage(),
+            title: "Search"
+        ), generateViewController(
+            rootViewController: ViewController(),
+            image: UIImage(named: "Library") ?? UIImage(),
+            title: "Library"
+        )]
+    }
+    
+    private func generateViewController(
+        rootViewController: UIViewController,
+        image: UIImage,
+        title: String
+    ) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+    }
+}
--- MusicApp/SearchViewController.swift
+++ MusicApp/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  MusicApp
 //
 //  Created by Maksim Grischenko on 07.10.2022.
@@ -7,14 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         view.backgroundColor = .white
     }
-
-
 }
-
