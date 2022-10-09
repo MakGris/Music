06e22687303ba3baa3fcd144be9abeb5c4795594
MakGris <maxgrishenko@gmail.com>
2022-10-09 13:35:21 +0700

    add Alamofire

--- MusicApp/SearchViewController.swift
+++ MusicApp/SearchViewController.swift
@@ -6,12 +6,64 @@
 //
 
 import UIKit
-
+import Alamofire
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tracks = [TrackModel(trackName: "bad guy", artistName: "Billie Eilish"),
+                 TrackModel(trackName: "bury a friend", artistName: "Billie Eilish")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setupSearchBar()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.rowHeight = 80
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let track = tracks[indexPath.row]
+        content.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        content.image = UIImage(named: "Image")
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        print(searchText)
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        AF.request(url).response { dataResponse in
+            if let error = dataResponse.error {
+                print("Error received requesting data:\(error.localizedDescription)")
+                return
+            }
+            guard let data = dataResponse.data else { return }
+            let someString = String(data: data, encoding: .utf8)
+            print(someString ?? "")
+        }
+        
     }
 }
