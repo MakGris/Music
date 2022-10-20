4a9d36c5eb80444c8ebd1d716b24b0ddfafdbae6
MakGris <maxgrishenko@gmail.com>
2022-10-20 19:04:13 +0700

    add minimized animation to player

--- MusicApp/MainTabBarController.swift
+++ MusicApp/MainTabBarController.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
+protocol MAinTabBarControllerDelegate: AnyObject {
+    func minimizeTrackDetailController()
+}
+
 class MainTabBarController: UITabBarController {
+    
+    let searchVC: SearchViewController = SearchViewController.loadFromStoryboard()
+    
+    private var minimizedTopAnchorConstraint: NSLayoutConstraint!
+    private var maximizedTopAnchorConstraint: NSLayoutConstraint!
+    private var bottomAnchorConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +30,7 @@ class MainTabBarController: UITabBarController {
             blue: 85/255,
             alpha: 1
         )
-        let searchVC: SearchViewController = SearchViewController.loadFromStoryboard()
+        setupTrackDetailVIew()
         
         viewControllers = [generateViewController(
             rootViewController: searchVC,
@@ -45,4 +55,57 @@ class MainTabBarController: UITabBarController {
         navigationVC.navigationBar.prefersLargeTitles = true
         return navigationVC
     }
+    
+   
+    
+    private func setupTrackDetailVIew() {
+        print("Тут мы будем настраивать TrackDetailView")
+        let trackDetailView: TrackDetailView = TrackDetailView.loadFromNib()
+        trackDetailView.backgroundColor = .green
+        trackDetailView.tabBarDelegate = self
+        trackDetailView.delegate = searchVC
+        view.insertSubview(trackDetailView, belowSubview: tabBar)
+        
+        trackDetailView.translatesAutoresizingMaskIntoConstraints = false
+        
+        maximizedTopAnchorConstraint = trackDetailView.topAnchor.constraint(
+            equalTo: view.topAnchor
+        )
+        minimizedTopAnchorConstraint = trackDetailView.topAnchor.constraint(
+            equalTo: tabBar.topAnchor,
+            constant: -64)
+        bottomAnchorConstraint = trackDetailView.bottomAnchor.constraint(
+            equalTo: view.bottomAnchor,
+            constant: view.frame.height
+        )
+        
+        bottomAnchorConstraint.isActive = true
+        maximizedTopAnchorConstraint.isActive = true
+        
+        trackDetailView.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor)
+        .isActive = true
+        trackDetailView.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor
+        ).isActive = true
+        
+    }
+}
+
+extension MainTabBarController: MAinTabBarControllerDelegate {
+   
+    func minimizeTrackDetailController() {
+        maximizedTopAnchorConstraint.isActive = false
+        minimizedTopAnchorConstraint.isActive = true
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 1,
+            options: .curveEaseInOut) {
+                self.view.layoutIfNeeded()
+            }
+    }
+    
+    
 }
--- MusicApp/Search/TrackDetailView/TrackDetailView.swift
+++ MusicApp/Search/TrackDetailView/TrackDetailView.swift
@@ -32,6 +32,7 @@ class TrackDetailView: UIView {
     }()
     
     weak var delegate: TrackMovingDelegate?
+    weak var tabBarDelegate: MAinTabBarControllerDelegate?
     
 //    MARK: - awakeFromNib
     
@@ -141,7 +142,8 @@ class TrackDetailView: UIView {
     }
     
     @IBAction func dragDownButtonTapped(_ sender: Any) {
-        self.removeFromSuperview()
+        tabBarDelegate?.minimizeTrackDetailController()
+//        self.removeFromSuperview()
     }
     
     @IBAction func previousTrack(_ sender: Any) {
